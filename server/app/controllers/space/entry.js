6eaaf536db443f2f9d436cb5967e7d6a02ccc441
worakon <worakon@forviz.com>
2017-05-11 15:33:39 +0700

    แก้ file

--- server/app/controllers/space/entry.js
+++ server/app/controllers/space/entry.js
@@ -8,6 +8,88 @@ const _helper = require('./helper');
 /**
  * Entries
  */
+
+const getQuery = async (q) => {
+    const queryString = {
+        "content_type": "contentTypeId",
+        'eq': '$eq',
+        'ne': '$ne',
+        'gt': '$gt',
+        'gte': '$gte',
+        'lt': '$lt',
+        'lte': '$lte',
+        'in': '$in',
+        'nin': '$nin'
+    };
+
+
+    const _q = {};
+    for (let index in q) {
+        const indexQuery = queryString[index] ? queryString[index] : index;
+        if (typeof (q[indexQuery]) === 'object') {
+            for (let __index in q[indexQuery]) {
+                if (queryString[__index]) {
+                    if (typeof (_q[indexQuery]) !== 'object') {
+                        _q[indexQuery] = {};
+                    }
+                    const tempVal = (__index === 'in' || __index === 'nin') ? q[indexQuery][__index].split(',') : q[indexQuery][__index];
+                    _q[indexQuery][queryString[__index]] = tempVal;
+                }
+            }
+        } else {
+            _q[indexQuery] = {
+                $eq: q[index]
+            };
+        }
+    }
+//    console.log(_q);
+
+    return _q;
+}
+
+exports.getEntry = async (req, res, next) => {
+//  const userOpenId = getIdentityFromToken(req);
+//  const user = await getUserFromIdentity(userOpenId);
+    try {
+        const spaceId = req.params.space_id;
+        const entryId = req.params.entry_id;
+
+        const reqQuery = req.query;
+        let select = "", skip = 0, limit = 0;
+        if (reqQuery.select) {
+            select = reqQuery.select;
+            delete reqQuery.select;
+        }
+        if (reqQuery.skip) {
+            skip = parseInt(reqQuery.skip);
+            delete reqQuery.skip;
+        }
+        if (reqQuery.limit) {
+            limit = parseInt(reqQuery.limit);
+            delete reqQuery.limit;
+        }
+
+        const _getQuery = await getQuery(reqQuery);
+
+        const _query = {
+            ..._getQuery,
+            _spaceId: {$eq: spaceId}
+        };
+        if (entryId)
+            _query["_id"] = {$eq: entryId};
+
+        console.log(_query);
+
+        const result = await Entry.find(_query).select(select).limit(limit).skip(skip);
+        res.json({
+            items: result,
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+
 exports.getAllEntries = (req, res, next) => {
     const spaceId = req.params.space_id;
     Space.findOne({_id: spaceId}).populate('entries').exec((err, space) => {
--- server/app/index.js
+++ server/app/index.js
@@ -39,7 +39,6 @@ const entryController = require('./controllers/space/entry');
 const apiKeyController = require('./controllers/space/apikey');
 const assetController = require('./controllers/space/asset');
 
-const testController = require('./controllers/test/');
 
 const cloudinaryController = require('./controllers/services/cloudinary');
 
@@ -173,11 +172,11 @@ const contentManagementAuthentication = jwt({
     algorithms: ['RS256']
 });
 
-
+ 
 const apiPrefix = '/v1';
 
-app.get(`${apiPrefix}/spaces/:space_id/entries`, contentDeliveryAuthentication, testController.getEntry);
-app.get(`${apiPrefix}/spaces/:space_id/entries/:entry_id`, contentDeliveryAuthentication, testController.getEntry);
+app.get(`${apiPrefix}/spaces/:space_id/entries`, contentDeliveryAuthentication, entryController.getEntry);
+app.get(`${apiPrefix}/spaces/:space_id/entries/:entry_id`, contentDeliveryAuthentication, entryController.getEntry);
 
 app.get(`${apiPrefix}/spaces/:space_id`, contentDeliveryAuthentication, spaceController.getSingle);
 app.get(`${apiPrefix}/spaces/:space_id/content_types`, contentDeliveryAuthentication, contentTypeController.getAllContentTypes);

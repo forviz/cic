22b7e66896a980075922125c25b4c60ce9a88678
breeze-pornthip <breeze.ki.ku@gmail.com>
2017-05-23 17:02:15 +0700

    update code or

--- server/app/controllers/space/index.js
+++ server/app/controllers/space/index.js
@@ -1,6 +1,6 @@
-import { getIdentityFromToken } from '../../utils/jwtUtils';
-
+const mongoose = require('mongoose');
 const _ = require('lodash');
+import { getAccessToken, decodeToken, getIdentityFromToken } from '../../utils/jwtUtils';
 const Space = require('../../models/Space');
 const User = require('../../models/User');
 const Organization = require('../../models/Organization');
@@ -16,8 +16,6 @@ export const getUserFromIdentity = async (identity) => {
 
     // Else create new one
     const newUser = new User();
-    newUser.email = '';
-
     const [provider, providerId] = _.split(identity, '|');
     newUser.identities = [
       {
@@ -81,9 +79,7 @@ exports.getAll = async (req, res, next) => {
   try {
     // const result = await Space.find({ users: user._id });
 
-    const userOrgazation = await Organization.find({$or: [
-      {$or : [{'users.Members':user._id},{'users.Owners':user._id}] }
-    ] });
+    const userOrgazation = await Organization.find({$or: [{'users.Members':user._id},{'users.Owners':user._id}] });
 
     const result = await Space.find({
           organization: { $in: _.map(userOrgazation, '_id') }
